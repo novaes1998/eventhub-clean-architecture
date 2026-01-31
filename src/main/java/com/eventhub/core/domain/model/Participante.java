3ee1eb217bca359561f752ca95b3e9450c7de15b
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-30 22:48:47 -0300

    feat: implementa entidade Participante com validação de email

---
+++ src/main/java/com/eventhub/core/domain/model/Participante.java
@@ -0,0 +1,25 @@
+package com.eventhub.core.domain.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+public class Participante {
+
+    private final UUID id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+
+    public Participante(UUID id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+}
