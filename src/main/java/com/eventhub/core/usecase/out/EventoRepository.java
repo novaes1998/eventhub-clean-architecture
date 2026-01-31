165d765c90947ee92a528a7a6a5a1eb17528b9c7
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-30 23:18:21 -0300

    feat: define porta de saída EventoRepository

---
+++ src/main/java/com/eventhub/core/usecase/out/EventoRepository.java
@@ -0,0 +1,20 @@
+package com.eventhub.core.usecase.out;
+
+import com.eventhub.core.domain.model.Evento;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface EventoRepository {
+
+    Optional<Evento> buscarPorId(UUID id);
+
+    List<Evento> listar();
+
+    Evento salvar(Evento evento);
+
+    Evento salvar(UUID id, Evento evento);
+
+    void deletar(UUID id);
+}
